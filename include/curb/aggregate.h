728d70a1810bee616307417386f637134e8fcfe9
Linh Nguyen <linh.nguyen@enway.ai>
2022-12-14 10:39:45 +0800

    Minor formatting cleanup

--- include/curb/aggregate.h
+++ include/curb/aggregate.h
@@ -1,14 +1,14 @@
-#ifndef AGGREGATE_H_
-#define AGGREGATE_H_
+#ifndef CURB_AGGREGATE_H_
+#define CURB_AGGREGATE_H_
 
-#include <ros/ros.h>
-#include <pcl_conversions/pcl_conversions.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl/point_types.h>
-#include <tf/transform_listener.h>
-#include <pcl_ros/point_cloud.h>
 #include <message_filters/subscriber.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/point_cloud.h>
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <tf/message_filter.h>
+#include <tf/transform_listener.h>
 
 typedef pcl::PointXYZI PointT;
 typedef pcl::PointCloud<PointT> CloudT;
@@ -16,14 +16,16 @@ typedef pcl::PointCloud<PointT> CloudT;
 class Aggregate
 {
 private:
-  ros::Publisher point_pub;
-  message_filters::Subscriber<sensor_msgs::PointCloud2> cloud2_sub_;
   std::string frame_id_;
   double factor_;
-  void pointCallback(const sensor_msgs::PointCloud2::ConstPtr& msg);
   CloudT::Ptr aggregated_cloud_;
+
+  ros::Publisher point_pub_;
+  message_filters::Subscriber<sensor_msgs::PointCloud2> cloud_sub_;
   tf::TransformListener tf_;
-  tf::MessageFilter<sensor_msgs::PointCloud2> *tf_filter_;
+  tf::MessageFilter<sensor_msgs::PointCloud2>* tf_filter_;
+
+  void pointCallback(const sensor_msgs::PointCloud2::ConstPtr& msg);
 
 public:
   Aggregate(ros::NodeHandle nh);
--- src/aggregate.cpp
+++ src/aggregate.cpp
@@ -1,6 +1,6 @@
-#include <pcl_ros/transforms.h>
-#include <pcl_ros/impl/transforms.hpp>
 #include <curb/aggregate.h>
+#include <pcl_ros/impl/transforms.hpp>
+#include <pcl_ros/transforms.h>
 
 Aggregate::Aggregate(ros::NodeHandle nh)
 {
@@ -8,15 +8,16 @@ Aggregate::Aggregate(ros::NodeHandle nh)
 
   nh_params.param("frame_id", frame_id_, std::string("odom"));
   nh_params.param("factor", factor_, 7.0);
-  cloud2_sub_.subscribe(nh, "input", 10);
-  tf_filter_ = new tf::MessageFilter<sensor_msgs::PointCloud2>(cloud2_sub_, tf_, frame_id_, 10);
+  cloud_sub_.subscribe(nh, "input", 10);
+  tf_filter_ = new tf::MessageFilter<sensor_msgs::PointCloud2>(cloud_sub_, tf_, frame_id_, 10);
   tf_filter_->registerCallback(boost::bind(&Aggregate::pointCallback, this, _1));
-  point_pub = nh.advertise<sensor_msgs::PointCloud2>("output", 10);
+  point_pub_ = nh.advertise<sensor_msgs::PointCloud2>("output", 10);
   CloudT::Ptr full_cloud(new CloudT());
   aggregated_cloud_ = full_cloud;
 }
 
-void Aggregate::pointCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
+void
+Aggregate::pointCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 {
   CloudT::Ptr cloud(new CloudT());
   pcl::fromROSMsg(*msg, *cloud);
@@ -24,7 +25,8 @@ void Aggregate::pointCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
 
   pcl_ros::transformPointCloud<PointT>(frame_id_, *cloud, *transformed, tf_);
   *aggregated_cloud_ += *transformed;
-  if (aggregated_cloud_->points.size() > transformed->points.size() * factor_) {
+  if (aggregated_cloud_->points.size() > transformed->points.size() * factor_)
+  {
     aggregated_cloud_->erase(aggregated_cloud_->points.begin(),
                              aggregated_cloud_->points.begin() + transformed->points.size());
   }
@@ -33,10 +35,11 @@ void Aggregate::pointCallback(const sensor_msgs::PointCloud2::ConstPtr& msg)
   pcl::toROSMsg(*aggregated_cloud_, out);
   out.header = msg->header;
   out.header.frame_id = frame_id_;
-  point_pub.publish(out);
+  point_pub_.publish(out);
 }
 
-int main(int argc, char* argv[])
+int
+main(int argc, char* argv[])
 {
   ros::init(argc, argv, "aggregate");
   ros::NodeHandle nh;
